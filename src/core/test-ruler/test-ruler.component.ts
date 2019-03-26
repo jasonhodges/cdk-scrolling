9329e56d1da3e7f0b82bb21b01237bd19df0d7aa
jasonhodges <jasonlhodges@gmail.com>
2019-03-26 10:41:09 -0600

    Utilize 'change()' Observable to dynamically update sizes.

--- src/core/test-ruler/test-ruler.component.ts
+++ src/core/test-ruler/test-ruler.component.ts
@@ -12,24 +12,44 @@ import { ViewportRuler } from '@angular/cdk/scrolling';
 export class TestRulerComponent implements OnInit {
   vpWidth: any = 0;
   widthSubtraction = 50;
-  contWidth = this.vpWidth - this.widthSubtraction;
-  
+  contWidth;
+  cont2Width;
+
   vpHeight: any = 0;
   heightSubtraction = 200;
-  contHeight = this.vpHeight - this.heightSubtraction;
+  contHeight;
+  cont2Height;
 
-  constructor(private vr: ViewportRuler) { }
+  changeSubscription$;
+
+  changeObserver = {
+    next: x => console.log('Viewport change detected'),
+    complete: () => this.setSize()
+  };
+
+  constructor(private vr: ViewportRuler) {
+    
+  }
 
   ngOnInit() {
+    this.setSize();
+    this.vr.change().subscribe(() => {
+      this.setSize();
+    });
+  }
+
+  setSize() {
     console.log(this.vr.getViewportSize());
+
     this.vpWidth = this.vr.getViewportSize().width;
     this.contWidth = this.vpWidth - this.widthSubtraction;
+    this.cont2Width = this.contWidth - 40;
 
     this.vpHeight = this.vr.getViewportSize().height;
     this.contHeight = this.vpHeight - this.heightSubtraction;
+    this.cont2Height = this.contHeight/4;
 
     console.log(this.vpWidth, this.vpHeight);
-    console.log(TestRulerComponent.prototype);
   }
 
 }
